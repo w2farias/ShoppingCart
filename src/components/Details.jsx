ecc65fcb7e1c5590ac9f2e5eb238776cdf6ad3c1
Wellington Farias <tonw2farias@gmail.com>
2024-04-03 10:18:53 -0300

    Projeto Shopping E-Commerce

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import logo from '../pages/Logo.gif'
+import { useNavigate } from 'react-router-dom'
 
 function Details() {
     const { id } = useParams()
     const [product, setProduct] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`https://fakestoreapi.com/products/${id}`)
@@ -15,12 +18,16 @@ function Details() {
     if (!product) return null
 
     return (
+
         <div>
+            <img width='250px' src={logo} alt="Logo" />
+            <br /><br />
             <h2>{product.title}</h2>
-            <img width='250px' src={product.image} alt={product.title} />
+            <img width='200px' src={product.image} alt={product.title} />
             <p>Preço: ${product.price}</p>
             <p >Descrição: {product.description}</p>
             <p>Avaliações: {product.rating.rate} ({product.rating.count} reviews)</p>
+            <button onClick={() => navigate(-1)}>Voltar</button>
         </div>
     )
 }
